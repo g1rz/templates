358305342096bffdc3396b9b3d5abb65c7d2bb2c
Alexandr Dementev <feralist21@gmail.com>
2022-06-27 17:39:02 +0800

    Добавил swiper slider

--- source/js/app.js
+++ source/js/app.js
@@ -27,4 +27,18 @@ $('document').ready(function () {
       $(this).find('.f-accordion__item-content').slideUp();
     }
   });
+
+  // Swiper slider
+  const swiper = new Swiper('.swiper', {
+    loop: true,
+
+    pagination: {
+      el: '.swiper-pagination',
+    },
+
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
 });
